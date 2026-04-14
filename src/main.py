e1c0efa51bed4a8b794188c14752fac47828136f
Kipung <nankipung@gmail.com>
2026-04-13 20:20:34 -0700

    main edit

--- src/main.py
+++ src/main.py
@@ -60,6 +60,8 @@ async def run() -> None:
         while True:
             # ── STATE 1: Wait for Bluetooth connection ──────────────────────
             uid, disconnected_event = await wait_for_active_user_async(mini)
+            # uid = "BEYAvvfuXVZYo4lLPE5KFKLakId2"
+            # disconnected_event = asyncio.Event()
             firebase.set_user(uid)
 
             while True:
